aedc045f8dd63569dad12a5a28659e0a4604d051
kuchartI <trachuk1807@gmail.com>
2022-10-19 02:23:35 +0300

    leetcode (improve) #38. Count and Say

--- src/main/java/leetcode/medium/CountAndSay.kt
+++ src/main/java/leetcode/medium/CountAndSay.kt
@@ -5,28 +5,27 @@ import java.lang.StringBuilder
 fun main() {
     println(countAndSay(2))
 }
+
 //38. Count and Say
 fun countAndSay(n: Int): String {
     if (n == 1) {
         return "1"
-    } else if (n > 1) {
-        val str = countAndSay(n - 1)
-        var ch = str[0]
-        var counter = 0
-        val resStr = StringBuilder()
-        for (i in str) {
-            if (ch == i) {
-                counter++
-            } else {
-                resStr.append(counter.toString() + ch)
-                ch = i
-                counter = 1
-            }
-        }
-        if (counter > 0) {
-            resStr.append(counter.toString() + ch)
-        }
-        return resStr.toString()
     }
-    return ""
+    val str = countAndSay(n - 1)
+    var ch = str[0]
+    var counter = 0
+    val resStr = StringBuilder()
+    for (i in str) {
+        if (ch == i) {
+            counter++
+        } else {
+            resStr.append(counter.toString() + ch)
+            ch = i
+            counter = 1
+        }
+    }
+    if (counter > 0) {
+        resStr.append(counter.toString() + ch)
+    }
+    return resStr.toString()
 }
