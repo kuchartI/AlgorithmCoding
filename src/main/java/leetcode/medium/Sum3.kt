f1a3771151655743ffa9e361457c649919f56dd5
kuchartI <trachuk1807@gmail.com>
2022-11-02 02:11:30 +0300

    leetcode fix #15. 3Sum

--- src/main/java/leetcode/medium/Sum3.kt
+++ src/main/java/leetcode/medium/Sum3.kt
@@ -4,26 +4,27 @@ package leetcode.medium
 fun main() {
     println(
         threeSum(
-            intArrayOf(-1,0,1,2,-1,-4)
+            intArrayOf(-1, 0, 1, 2, -1, -4)
         )
     )
 }
 
 fun threeSum(nums: IntArray): List<List<Int>> {
     val repNums = ArrayList<Int>()
-    var repMap: LinkedHashMap<Int, Int> = LinkedHashMap()
+    val repMap: HashMap<Int, Int> = HashMap()
     for (i in nums) {
-        if (repMap.containsKey(i) && repMap[i]!! < 3 ) {
+        if (repMap.containsKey(i) && repMap[i]!! < 3) {
             repMap[i]?.plus(1)?.let { repMap.put(i, it) }
-        } else if(!repMap.containsKey(i)){
+        } else if (!repMap.containsKey(i)) {
             repMap[i] = 1
         }
     }
-    for(i in repMap){
-        for(j in 0 until i.value){
+    for (i in repMap) {
+        for (j in 0 until i.value) {
             repNums.add(i.key)
         }
     }
+    repNums.sort()
     val result: HashSet<ArrayList<Int>> = HashSet()
     val n = repNums.size
     for (i in 0 until n) {
